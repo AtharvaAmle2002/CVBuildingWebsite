7940108f700c29ce2e72ed4130f5657732ba2bc5
ATHARVA AMLE <98222833+AtharvaAmle2002@users.noreply.github.com>
2023-04-09 06:27:40 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/m.css">
+    <title>CV BUILDER</title>
+</head>
+<body>
+    <div class="navigation">
+        <span class="logo">
+            <img src="images/logo.png" alt="" class="first">
+        </span>
+           <span class="name">ProCV</span>
+            <span class="nav">
+                <a href="built.php">BUILD</a>
+                <a href="ABOUTUS.php">ABOUT US</a>
+                <a href="tips.php">TIPS</a>
+                <a href="login.php">LOG IN</a>
+            </span>
+    </div>
+    <div class="sec1">
+        <h1 class="imp">What is <span>ProCv ?</span></h1>
+        <!-- <p class="imp"><em>"First impression is the Last Impression"</em> -->
+        <p>We help you in desgining your perfect Cv</p>
+        <p>A Good CV must have proper format and must be error free</p>
+        <p>Choose from prebuilt templates</p>
+    </div>
+    <div class="sec2">
+        <h1>Design Elegant Cv</h1>
+        <h2>In just 3 simple steps</h2>
+        <p class="txt" id="txt1"><span>1.</span>Select One Template</p>
+        <!-- <img src="images/serchcv.png" alt="" class="search"> -->
+        <!-- <img src="images/form.jpg" alt="" class="form"> -->
+        <p class="txt" id="txt2"><span>2.</span>Fill your details in a Form</p>
+        <!-- <img src="images/compcv.png" alt="" class="compcv">  -->
+        <p class="txt" id="txt3"><span>3.</span>Download/Print your Cv</p>
+    </div>
+    <div class="sec3">
+        <div>
+        <!-- <img src="images/cvpic.jpg" alt="" class="cvpic"> -->
+         <p class="head">Get Shortlisted By Recruiters</p>
+         <p class="data">
+            <p>CVs are generally checked by automated programs, before seen by a Recruiters</p>
+            <p>Only CVs that are free of gramatical errors, properlly formated are selected</p>
+             <p>Using ProCv You can create CVs that get selected by automated Programs</p>
+         </p>
+         <a href="built.html"><button class="btn" type="button">GET STARTED</button></a>
+        </div>
+        <img src="images/candidateCv.png" alt="">
+    </div>
+</body>
+</html>
